10285d628a170d4e3c2a3a2308d872193074ad1a
Swapnaja Yadav <swapnajayadav2021@gmail.com>
2025-07-31 20:59:55 +0530

    Create a form for adding remarks

---
+++ comments/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import CommentsForm from './CommentsForm'
+
+export default function App() {
+  return (
+    <>
+      <CommentsForm/>
+    </>
+  )
+}
