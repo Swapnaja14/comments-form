77f0d4bcf1eb64cff069b450124df33a5414ef16
Swapnaja Yadav <swapnajayadav2021@gmail.com>
2025-08-03 15:37:04 +0530

    Add validations to CommentsForm using formik

--- comments/src/CommentsForm.jsx
+++ comments/src/CommentsForm.jsx
@@ -1,69 +1,121 @@
 import { useState } from "react"
 import './CommentsForm.css'
+import { useFormik } from 'formik';
 
-export default function CommentsForm ({addNewComment}) {
-    let [formData, setFormData] = useState({
-        username: "",
-        remarks: "",
-        rating: 5,
+const validate = values => {
+    const errors = {};
+    if (!values.username) {
+        errors.username = 'Username cannot be null';
+    }
+
+    if (!values.remarks) {
+        errors.remarks = 'Remarks cannot be null';
+    }
+
+    if (!values.rating) {
+        errors.rating = 'Rating cannot be null';
+    }
+
+    return errors;
+};
+
+
+export default function CommentsForm({ addNewComment }) {
+    // let [formData, setFormData] = useState({
+    //     username: "",
+    //     remarks: "",
+    //     rating: 5,
+    // });
+
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            remarks: '',
+            rating: 5,
+        },
+        validate,
+        onSubmit: values => {
+            addNewComment(values);
+            formik.resetForm(); // optional
+        }
     });
 
-    let handleInputData = (event) => {
-        setFormData((currData) => {
-            return {...currData, [event.target.name] : event.target.value};
-        });
-    };
+    // let handleInputData = (event) => {
+    //     setFormData((currData) => {
+    //         return {...currData, [event.target.name] : event.target.value};
+    //     });
+    // };
 
-    let handleSubmit = (event) => {
-        event.preventDefault();
-        addNewComment(formData)
-        console.log(formData);
-        setFormData({
-            username: "",
-            remarks: "",
-            rating: 5
-        });
-    };
+
+    // let handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     addNewComment(formData)
+    //     console.log(formData);
+    //     setFormData({
+    //         username: "",
+    //         remarks: "",
+    //         rating: 5
+    //     });
+    // };
 
     return (
         <div className="comments-form">
             <h4>Gives a comment</h4>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="username">Enter your username</label>
-                <input 
-                placeholder="username" 
-                type="text"
-                value={formData.username}
-                onChange={handleInputData}
-                name="username"
-                id="username"/>
+                <input
+                    placeholder="username"
+                    type="text"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    name="username"
+                    id="username"
+                />
+                {/* {formik.errors.username ? <div>{formik.errors.username}</div> : null} */}
+                {formik.touched.username && formik.errors.username ? (
+                    <div className="error-message">{formik.errors.username}</div>
+                ) : null}
+
 
                 <br></br><br></br>
 
                 <label htmlFor="remarks">Add your remarks- </label>
-                <textarea 
-                placeholder="Add Remarks"
-                value={formData.remarks}
-                onChange={handleInputData}
-                name="remarks"
-                id="remarks"></textarea>
+                <textarea
+                    placeholder="Add Remarks"
+                    value={formik.values.remarks}
+                    onChange={formik.handleChange}
+                    name="remarks"
+                    id="remarks"></textarea>
+                {formik.touched.remarks && formik.errors.remarks ? (
+                    <div className="error-message">{formik.errors.remarks}</div>
+                ) : null}
+
 
                 <br></br><br></br>
 
                 <label htmlFor="rating">Give your ratings- </label>
-                <input 
-                placeholder="rating" 
-                type="number" 
-                min={1} 
-                max={5}
-                value={formData.rating}
-                onChange={handleInputData}
-                name="rating"
-                id="rating"/>
+                <input
+                    placeholder="rating"
+                    type="number"
+                    min={1}
+                    max={5}
+                    value={formik.values.rating}
+                    onChange={formik.handleChange}
+                    name="rating"
+                    id="rating" />
+                {formik.touched.rating && formik.errors.rating ? (
+                    <div className="error-message">{formik.errors.rating}</div>
+                ) : null}
+
 
                 <br></br><br></br>
 
-                <button onClick={handleSubmit}>Add Comment</button>
+                <button
+                    // onClick={handleSubmit}
+                    type="submit"
+                >
+                    Add Comment</button>
             </form>
         </div>
     )
